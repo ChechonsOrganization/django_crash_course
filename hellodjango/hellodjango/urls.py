d5d05df873a1c2ccace10487345fe25f64ce00bc
Sergio Ramírez <sergio.04.ramirez@gmail.com>
2021-05-02 22:32:25 -0400

    add templates folder and add index.html

--- hellodjango/hellodjango/urls.py
+++ hellodjango/hellodjango/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+# import view from homepage app
+from homepage.views import HomepageView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomepageView.as_view(), name='home'),
 ]
--- hellodjango/homepage/views.py
+++ hellodjango/homepage/views.py
@@ -1,1 +1,15 @@
 from django.views.generic import TemplateView
+
+class HomepageView(TemplateView):
+    template_name = 'index.html'
+
+    # variables used in the template
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['my_statement'] = 'Nice to see you!'
+        context['testing'] = 'Este es un texto en español traido desde get_context_data'
+        context['suma'] = 40+40
+        return context
+
+    def say_bye(self):
+        return 'Goodbye'
